9b30997709ede4cdb25343e0ba06641389678731
mxork <>
2025-03-11 19:32:16 -0400

    fixup address basing for real windows

--- hooks/windows/patcher.c
+++ hooks/windows/patcher.c
@@ -6,16 +6,89 @@
 #include <stddef.h>
 
 // #include <sys/mman.h>
+#include <windows.h>
 #include <memoryapi.h>
 #include <processthreadsapi.h>
 #include <libloaderapi.h>
 
 #include "patcher.h"
 
-void unprotect(void* addr, size_t size);
-void protect(void* addr, size_t size);
+#include <stdarg.h>
 
-// #pragma comment(linker, "/SECTION:.shared,RWS")
+// this is a little unsafe, but man am I salty.
+// trying to write to the console is just not happening.
+bool log_output_open_once = false;
+FILE* log_output = NULL;
+void _log(const char *format, ...) {
+  if (!log_output_open_once) {
+    log_output_open_once = true;
+    log_output = fopen("patcher.log", "w");
+    if (!log_output) {
+        perror("Failed to open patcher log file");
+        exit(1); // :todo consider not doing this in release
+    }
+  }
+
+  if (!log_output) {
+    return;
+  }
+
+  va_list args;
+  va_start(args, format);
+  vfprintf(log_output, format, args);
+  va_end(args);
+  fflush(log_output);
+}
+
+void dumphex(uint8_t* start, size_t len) {
+    debug("  ");
+    for (int i=0; i<len; i++)
+      debug("%02x", *(start+i));
+    debug("\n");
+}
+
+__attribute__((constructor))
+void on_load() {
+  libpatcher_init();
+}
+
+enum patcher_mode _mode = normal;
+void libpatcher_init() {
+  info("libpatcher_init\n");
+
+  char* envvar = getenv("PATCHER_MODE");
+  if (envvar && *envvar) {
+    if (strcmp(envvar, "normal") == 0) {
+      info("mode: normal\n");
+      _mode = normal;
+    } else if (strcmp(envvar, "disabled") == 0) {
+      info("mode: disabled\n");
+      _mode = normal;
+      _mode = disabled;
+      return;
+    } else if (strcmp(envvar, "force_enabled") == 0) {
+      info("mode: force enabled\n");
+      _mode = force_enabled;
+    } else if (strcmp(envvar, "patched_but_disabled") == 0) {
+      info("mode: patched but disabled\n");
+      _mode = patched_but_disabled;
+    } else {
+      err("PATCHER_MODE: expected mode. got %s\n", envvar);
+      exit(1);
+    }
+  }
+
+  dopatch();
+}
+
+void on_nakama_client_join_match() {
+  if (_mode == normal) {
+    info("patcher normal mode: on nakama client join match, so enabling patch\n");
+    set_enabled(true);
+  }
+}
+
+#pragma comment(linker, "/SECTION:.shared,RWS")
 #pragma data_seg(".shared")
 atomic_bool enabled0 = false;
 #pragma data_seg()
@@ -27,73 +100,41 @@ bool get_enabled() {
   return atomic_load(enabled);
 }
 
-#define log(...) fprintf (stderr, __VA_ARGS__)
-void dumphex(uint8_t* start, size_t len) {
-    log("  ");
-    for (int i=0; i<len; i++)
-      log("%02x", *(start+i));
-    log("\n");
-}
+// atomic_bool enabled0;
+// atomic_bool* enabled = &enabled0;
+// void alloc_enabled() {
+//   atomic_init(enabled, false);
+// }
+// void set_enabled(bool value) {
+//   atomic_store(enabled, value);
+// }
+// bool get_enabled() {
+//   return atomic_load(enabled);
+// }
 
-__attribute__((constructor))
-void on_load() {
-  fprintf(stderr, "onload\n");
-  fflush(stderr);
-
-  char* disabled_envvar = getenv("DISABLED");
-  if (disabled_envvar && *disabled_envvar) {
-    log("DISABLED is present; refusing to patch\n");
-    return;
-  }
-
-  char* patched_disabled_envvar = getenv("PATCHED_BUT_DISABLED");
-  if (patched_disabled_envvar && *patched_disabled_envvar) {
-    log("PATCHED_BUT_DISABLED is present; disabling\n");
-    disabled_from_env = true;
-  }
-
-  dopatch();
-  set_enabled(true);
-}
-
-void dummy() {
-  fprintf(stderr, "dummy called\n");
-  fflush(stderr);
-}
-
-const void** currentGameState = (const void**)0x14233fe78;
 int get_current_day_of_month() {
-  const void* state = *currentGameState;
-  // log("current game state: %p\n", state);
+  // void** currentGameStateOffset = (void**)0x00233fe78;
+  const void* state = *(void**)(module_base+0x233fe78);
   const int currentDayOfMonthOffset = 0x1de4;
-  // log("  +%02x  : %d\n", 0x1dd0, *((int32_t*)(state+0x1dd0)));
-  // log("  +%02x  : %d\n", 0x1dd4, *((int32_t*)(state+0x1dd4)));
-  // log("  +%02x  : %d\n", 0x1ddc, *((int32_t*)(state+0x1ddc)));
-  // log("  +%02x  : %d\n", 0x1de4, *((int32_t*)(state+0x1de4)));
   int32_t current_day = *((int32_t*)(state+currentDayOfMonthOffset));
   return current_day;
 }
 
-bool disabled_from_env = false;
-// :todo I need to double check if I got the boolean condition backwards
 bool dynamic_should_run_hook() {
-  // log("dynamic should run running\n");
-
-  // actually day-1
-  int day = 1+get_current_day_of_month();
-
-  // bool should_run = get_enabled() && ((day % 7) != 0);
-  // bool should_run = false && ((day % 5) != 0);
-  // bool should_run = false;
-  // log("should run (%d): %s\n", day, should_run ? "yes" : "no");
-  // return true;
-  bool ret = !disabled_from_env && get_enabled() && (day % 7 != 0);
-  if (!ret) {
-    log("checksum run on day: %d\n", day);
+  switch (_mode) {
+  case normal:
+    int day = 1+get_current_day_of_month();
+    bool ret = get_enabled() && (day % 7 != 0);
+    return ret;
+  case force_enabled:
+    return true;
+  case disabled:
+    assert(false && "dynamic_should_run_hook should never be called if patch wasn't applied");
+    return false;
+  case patched_but_disabled:
+    return false;
   }
-  return ret;
 }
-
 
 void hook1();
 void* hook1return;
@@ -103,6 +144,7 @@ void* hook2return;
 
 void hook3();
 void* hook3return;
+void* hook3returnalt;
 
 #define jumplen 14
 void jump_inject(void* target, void* addr) {
@@ -129,23 +171,23 @@ void do1patch(
     void* hook,
     void** hook_return
 ) {
-  log("patching %p.\n size: %u\n hook: %p\n hook_return (addr): %p\n", target, patchsize, hook, hook_return);
+  debug("patching %p.\n size: %u\n hook: %p\n hook_return (addr): %p\n", target, patchsize, hook, hook_return);
 
   // tell the hook where to return to
   *hook_return = target+jumplen-1; // -1 is the pop rax
-  log("hook return (value): %p\n", *hook_return);
+  debug("hook return (value): %p\n", *hook_return);
 
   // unprotect functions
   unprotect(target, patchsize);
 
   // clobber target and install jump
   assert((patchsize >= jumplen) && "not enough room for jump");
-  log("target (pre) ");
+  debug("target (pre) ");
   dumphex(target, patchsize);
 
   memset(target, 0x90, patchsize);
   jump_inject(target, hook);
-  log("target (post): ");
+  debug("target (post): ");
   // dumphex(target, patchsize);
   dumphex(target, patchsize);
 
@@ -170,13 +212,16 @@ void do1patch(
 //   [in] HMODULE hModule,
 //   [in] LPCSTR  lpProcName
 // );
+void* module_base;
 void dopatch() {
   HMODULE module;
   GetModuleHandleExA(0, NULL, &module);
   // technically, we could just pass null to get the name
   char name[256];
   GetModuleFileNameA(module, name, 256);
-  log("dopatch. main exe name: %s\n", name);
+  debug("dopatch. main exe name: %s\n", name);
+  module_base = (void*) module;
+  debug("  module base: %p\n", module_base);
 
   // from https://stackoverflow.com/questions/26572459/c-get-module-base-address-for-64bit-application
   // I'm 90% sure that the module handle is, itself, a pointer to the base address of the module.
@@ -194,17 +239,18 @@ void dopatch() {
   // SDL_vsnprintf addr:         00000001417361A0
   // SDL_vsnprintf addr (orig):  0x17361a0
   FARPROC thaddr = GetProcAddress(module, "SDL_vsnprintf");
-  log("module handle:              %p\n", module);
-  log("SDL_vsnprintf addr:         %p\n", thaddr);
-  log("SDL_vsnprintf addr (orig):  %s\n", "0x17361a0");
+  debug("module handle:              %p\n", module);
+  debug("SDL_vsnprintf addr:         %p\n", thaddr);
+  debug("SDL_vsnprintf addr (orig):  %s\n", "0x17361a0");
   assert(((((void*)thaddr)-((void*)module)) == 0x17361a0) && "address of SDL_vsnprintf didn't match expected");
 
   // now dump some bytes and check they match
   // calc checksums 0x140743aa0
   // huh. I didn't realize this shit was static.
   // 0x488954241048894c is expected, but little-endian man
-  void* calcchecksums_addr = (void*)0x140743aa0;
-  log("calcchecksums_addr: %p\n", calcchecksums_addr);
+  // void* calcchecksums_addr = (void*)0x140743aa0;
+  void* calcchecksums_addr = module_base + 0x743aa0;
+  debug("calcchecksums_addr: %p\n", calcchecksums_addr);
   dumphex(calcchecksums_addr, 10);
   assert((*((uint64_t*)calcchecksums_addr) == 0x4c89481024548948) && "bytes at calcchecksums_addr did not match expected");
 
@@ -228,13 +274,13 @@ void dopatch() {
 
   //unprotect(calcchecksums_unit_loop_target, patchsize);
 
-  //log("unit loop pre: %p\n", calcchecksums_unit_loop_target);
+  //debug("unit loop pre: %p\n", calcchecksums_unit_loop_target);
   //dumphex(calcchecksums_unit_loop_target, patchsize);
 
   //memset(calcchecksums_unit_loop_target, 0x90, patchsize);
   //memcpy(calcchecksums_unit_loop_target, patch, 7); // sizeof(patch));
   //                                                  //
-  //log("unit loop post: %p\n", calcchecksums_unit_loop_target);
+  //debug("unit loop post: %p\n", calcchecksums_unit_loop_target);
   //dumphex(calcchecksums_unit_loop_target, patchsize);
 
   //protect(calcchecksums_unit_loop_target, patchsize);
@@ -247,8 +293,8 @@ void dopatch() {
   // mov rax, qword [rdi+0x1c98]
   // sub rax, qword [rdi+0x1c90]
   // sar rax, 0x3
-  do1patch((void*)0x140743e3a, 18, hook1, &hook1return);
-  do1patch((void*)0x140744461, 18, hook2, &hook2return);
+  do1patch(module_base + 0x743e3a, 18, hook1, &hook1return);
+  do1patch(module_base + 0x744461, 18, hook2, &hook2return);
 
 
 
@@ -261,7 +307,8 @@ void dopatch() {
   // jle 0x1407458f5
   //
   // skip loop addr 0x1407458f5
-  do1patch((void*)0x140745884, 14, hook3, &hook3return);
+  do1patch(module_base + 0x745884, 14, hook3, &hook3return);
+  hook3returnalt = module_base + 0x7458f5;
 }
 
 // BOOL VirtualProtect(
@@ -277,36 +324,33 @@ void dopatch() {
 // [in] SIZE_T  dwSize
 // );
 void unprotect(void* addr, size_t size) {
-  log("  unprotect\n");
-  log("    addr: %p\n", addr);
-  log("    size: %u\n", size);
+  debug("  unprotect\n");
+  debug("    addr: %p\n", addr);
+  debug("    size: %u\n", size);
   DWORD old_protect;
   DWORD new_protect = PAGE_EXECUTE_READWRITE;
 
   if (VirtualProtect(addr, size, new_protect, &old_protect) == 0) {
-      perror("unprotect failed");
-      fflush(stderr);
+      err("unprotect failed");
       exit(1);
   }
 }
 
 void protect(void* addr, size_t size) {
-  log("  unprotect\n");
-  log("    addr: %p\n", addr);
-  log("    size: %u\n", size);
+  debug("  unprotect\n");
+  debug("    addr: %p\n", addr);
+  debug("    size: %u\n", size);
   DWORD old_protect;
   DWORD new_protect = PAGE_EXECUTE_READ;
 
   if (VirtualProtect(addr, size, new_protect, &old_protect) == 0) {
-      perror("protect failed");
-      fflush(stderr);
+      err("protect failed");
       exit(1);
   }
 
   HANDLE handle = GetCurrentProcess();
   if (FlushInstructionCache(handle, addr, size) == 0) {
-      perror("flush instruction cache failed");
-      fflush(stderr);
+      err("flush instruction cache failed");
       exit(1);
   }
 }
--- hooks/windows/patcher.h
+++ hooks/windows/patcher.h
@@ -2,7 +2,6 @@
 #pragma once
 extern "C" {
 #else
-// c++ explodes on atomics; it's internal, anyways
 #include <stdatomic.h>
 extern atomic_bool* enabled; // changes visible to threads, need to alloc
 #endif
@@ -11,20 +10,32 @@ extern atomic_bool* enabled; // changes visible to threads, need to alloc
 #include <stddef.h>
 
 // public
-void dopatch();
-bool dynamic_should_run_hook();
-void dummy();
+void libpatcher_init();
+void on_nakama_client_join_match();
+void _log(const char* format, ...);
 
+#define log(...) _log (__VA_ARGS__)
+#define info(...) _log (__VA_ARGS__)
+#define debug(...) _log (__VA_ARGS__)
+// #define debug(...)
+#define err(...) _log (__VA_ARGS__)
+
+// internal
+bool dynamic_should_run_hook();
+extern void* module_base;
 extern void* hook1return;
 extern void* hook2return;
 extern void* hook3return;
+extern void* hook3returnalt;
 
-// internal
-extern bool disabled_from_env;
+void dopatch();
+enum patcher_mode {normal, disabled, force_enabled, patched_but_disabled};
+extern enum patcher_mode _mode;
 void set_enabled(bool value);
 bool get_enabled();
-void alloc_enabled();
 void do1patch( void* target, size_t patchsize, void* hook, void** hook_return);
+void unprotect(void* addr, size_t size);
+void protect(void* addr, size_t size);
 
 #ifdef __cplusplus
 }
